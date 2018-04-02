480e5e6588389175d57b64c6fe3fdbad75bd9f9d
Yvon Liu <yvon_liu@trend.com.tw>
2018-04-02 14:51:24 +0800

    add templated resource acq-del

--- testGeneric/main.cpp
+++ testGeneric/main.cpp
@@ -7,21 +7,42 @@
 using namespace std;
 
 class useless {};
-
-useless* r1_acquire(int a1) { cout << "r1_a(" << a1 << endl; return new useless; }
+class bad_ssl_resource : public std::runtime_error
+{
+public:
+    bad_ssl_resource() = delete;
+    bad_ssl_resource(const string & error) : std::runtime_error( error + " - bad_ssl_resource ") {}    
+};
+useless* r1_acquire(int a1) 
+{
+    cout << "r1_a(" << a1 << endl; 
+    if (a1 == 0) 
+        return nullptr; 
+    else
+        return new useless; 
+}
 void r1_delete(useless* r1) { cout << "r1_d(" << r1 << endl; delete r1; }
 
-template<typename T, typename Deletion, typename Acquisition, typename...Args>
-std::unique_ptr<T, Deletion> create_unique_resource(Acquisition acquisition, Deletion deletion, Args&&...args)
+template<typename ResourceType, typename Uninit, typename Init, typename...InitArgs>
+std::unique_ptr<ResourceType, Uninit> create_unique_resource(Init init, Uninit uninit, InitArgs&&...args)
 {
-    return {acquisition(std::forward<Args>(args)...), deletion};
+    ResourceType * o = init(std::forward<InitArgs>(args)...);
+    if (NULL == o) throw bad_ssl_resource(typeid(ResourceType).name());
+    return {o, uninit};
 }
-
 int main()
 {
     cout << "scope {" << endl;
+    try
     {
-        auto r = create_unique_resource<useless>(r1_acquire, r1_delete, 100);
+        auto w = create_unique_resource<useless>(r1_acquire, r1_delete, 10);
+        auto x = create_unique_resource<useless>(r1_acquire, r1_delete, 10);
+        auto y = create_unique_resource<useless>(r1_acquire, r1_delete, 0);
+        auto z = create_unique_resource<useless>(r1_acquire, r1_delete, 100);
+    }
+    catch(const std::runtime_error & e)
+    {
+        cout << "runtime error:" << e.what() << endl;;
     }
 
     cout << "}" << endl;
